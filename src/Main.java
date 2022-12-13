25f0d5f7990263cdcce332db18bd983635d3db8c
Kseniya <okorokova.kseniya@gmail.com>
2022-12-13 14:06:22 +0300

    solution added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    int initialAccount = 100;
+    int added = 1100;
+    int bonus = 11;
+    if(added > 1100)
+
+    {
+        bonus = added / 100;
     }
+
+    int finalAccount = initialAccount + added + bonus;
+
+    System.out.println("Итоговый счет: " + finalAccount);
+    System.out.println("Бонус: " + bonus);
+  }
 }
